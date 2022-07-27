a63d0fbb27f3ea6485cc2af957a6d73c58db7d75
Connie Park <conniedc1206@gmail.com>
2022-07-26 19:57:07 -0700

    commit tree 5c6cf93d219599a05b75fc54abac05bb997668a8

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,16 @@
+class SessionsController < ApplicationController
+    # 2. create sessions controller using rails g controller Sessions --no-test-framework
+    
+    # 3. Find a user in the database using the username from params, Save the user's ID to the session hash, Return the user as a JSON object
+    def create
+        user = User.find_by(username: params[:username])
+        session[:user_id] = user.id
+        render json: user, status: :created
+    end
+    
+    # 4. Remove the user ID from the session hash, Return an empty response with a 204 No Content status code
+    def destroy
+        session.delete :user_id
+        head :no_content
+    end
+end
